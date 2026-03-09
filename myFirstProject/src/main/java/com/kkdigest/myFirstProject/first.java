c832c0ec2d1d0603634880d8dcd968a140c84cc6
Krishnakant Tiwari <122863169+krishnakanttiwariSecU32@users.noreply.github.com>
2026-03-09 16:05:11 +0530

    Initial commit

--- myFirstProject/src/main/java/com/kkdigest/myFirstProject/first.java
+++ myFirstProject/src/main/java/com/kkdigest/myFirstProject/first.java
@@ -1,12 +1,17 @@
 package com.kkdigest.myFirstProject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class first {
-    @GetMapping("kk")
-    public String greeting() {
-        return "Hello KK !";
+
+    @Autowired //It is used for the dependency injection it injects the dependencies of another class
+    private Dog dog;  // it is the dependency of the dog class as a field
+
+    @GetMapping("/kk")
+    public String ok(){
+        return Dog.fun();
     }
 }
